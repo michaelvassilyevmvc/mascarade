2b1d6b9d2a356a03f66e4b9bcf267ef25225ff47
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2026-06-09 14:16:42 +0500

    Настройка доступа через yarp-gateway

--- mascarade.IdentityService/Config.cs
+++ mascarade.IdentityService/Config.cs
@@ -28,6 +28,17 @@ public static class Config
                 RedirectUris = { "http://getpostman.com/oauth2/callback" },
                 ClientSecrets = { new Secret("NotASecret".Sha256()) },
                 AllowedGrantTypes = { GrantType.ResourceOwnerPassword }
+            },
+            new Client
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                ClientSecrets = {new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = {"http://localhost:3000/api/auth/callback/id-server"},
+                AllowedScopes = {"openid","profile","mascaradeApp"},
+                AccessTokenLifetime = 3600*24*30
             }
         };
 }
